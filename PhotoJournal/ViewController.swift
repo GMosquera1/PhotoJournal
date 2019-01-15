56cf0527e76e12d5b86941246d89f6d2ecfd1dfe
GMosquera1 <genesismosquera@pursuit.org>
2019-01-14 19:30:46 -0500

    initial commit

---
+++ PhotoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Genesis Mosquera on 1/14/19.
+//  Copyright © 2019 Genesis Mosquera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
